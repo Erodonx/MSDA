a72d175d6d23922bb6ef6e687bdb7bdcc5d84c12
Stal Charles <stalcha01@gmail.com>
2024-01-25 15:16:56 +0100

    test

---
+++ migrations/Version20240125133646.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240125133646 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(50) NOT NULL, image VARCHAR(50) NOT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, date_commande DATE NOT NULL, total NUMERIC(6, 2) NOT NULL, etat INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE detail (id INT AUTO_INCREMENT NOT NULL, quantite INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE plat (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, prix NUMERIC(5, 2) NOT NULL, image VARCHAR(50) NOT NULL, is_active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE utilisateur ADD nom VARCHAR(50) NOT NULL, ADD prenom VARCHAR(50) NOT NULL, ADD telephone VARCHAR(20) NOT NULL, ADD adresse VARCHAR(50) NOT NULL, ADD cp VARCHAR(20) NOT NULL, ADD ville VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE commande');
+        $this->addSql('DROP TABLE detail');
+        $this->addSql('DROP TABLE plat');
+        $this->addSql('ALTER TABLE utilisateur DROP nom, DROP prenom, DROP telephone, DROP adresse, DROP cp, DROP ville');
+    }
+}
---
+++ src/Controller/ContactController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Entity\Utilisateur;
+use App\Form\DemoFormType;
+use App\Form\ContactFormType;
+use App\Service\MailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+//use Symfony\Component\Mime\Email;
+
+
+class ContactController extends AbstractController
+{
+    //#[isGranted('ROLE_CLIENT', message: "Nous sommes désolés, vous ne disposez pas des autorisations nécessaires pour accèder à cette page!")]
+    #[Route('/contact', name: 'app_contact')]
+    public function index(#[CurrentUser] ?Utilisateur $user,Request $request, EntityManagerInterface $entityManager, MailService $ms, ): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //on crée une instance de Contact
+            // $message = new Contact();
+            // Traitement des données du formulaire
+            $message = $form->getData();
+            //on stocke les données récupérées dans la variable $message
+            // $message = $data;
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+            //envoi de mail avec notre service MailService
+            $email = $ms->sendMail( $user->getEmail(), $message->getEmail(), $message->getObjet(), $message->getMessage() );
+//            dd($message->getEmail());
+return $this->redirect("/");
+        }
+
+
+        return $this->render('contact/index.html.twig', [
+//            'form' => $form->createView(),
+              'form' => $form
+        ]);
+    }
+}
